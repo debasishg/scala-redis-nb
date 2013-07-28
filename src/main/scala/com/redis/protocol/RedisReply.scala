617a352f8004c671f5e8c85e4b609fcf64171557
Jisoo Park <xxxyel@gmail.com>
2013-07-28 23:29:29 +0900

    Remove temporary logger

--- src/main/scala/com/redis/protocol/RedisReply.scala
+++ src/main/scala/com/redis/protocol/RedisReply.scala
@@ -1,7 +1,6 @@
 package com.redis.protocol
 
 import com.redis.serialization.Parse
-import org.slf4j.LoggerFactory // @TODO remove after bugfix
 
 /**
  * Redis will reply to commands with different kinds of replies. It is always possible to detect the kind of reply
@@ -13,32 +12,23 @@ import org.slf4j.LoggerFactory // @TODO remove after bugfix
  * <li> In a Multi Bulk Reply the first byte of the reply s "*"</li>
  */
 
-sealed trait RedisReply[T] { self =>
-
-  protected def log = LoggerFactory.getLogger(self.getClass) // @TODO remove after bugfix
+sealed trait RedisReply[T] {
 
   def value: T
-
   def asAny = value.asInstanceOf[Any]
 
   def asLong: Long =  ???
-
   def asString: String = ???
-
   def asBulk[A: Parse]: Option[A] = ???
-
   def asBoolean: Boolean = ???
-
   def asList[A: Parse]: List[Option[A]] = ???
-
   def asListPairs[A: Parse, B: Parse]: List[Option[(A,B)]] = ???
-
   def asSet[A: Parse]: Set[A] = ???
 }
 
 case class IntegerReply(value: Long) extends RedisReply[Long] {
-  override final def asLong = value
-  override final def asBoolean: Boolean = value > 0
+  final override def asLong = value
+  final override def asBoolean: Boolean = value > 0
 }
 
 case class StatusReply(value: String) extends RedisReply[String] {
@@ -48,11 +38,7 @@ case class StatusReply(value: String) extends RedisReply[String] {
 
 case class BulkReply(value: Option[String]) extends RedisReply[Option[String]] {
   final override def asBoolean: Boolean = value.isDefined
-  final override def asBulk[A: Parse]: Option[A] = {
-    val parse = implicitly[Parse[A]]
-    if (parse == null) log.warn("Implicit parse for {} in asBulk[T: Parse] is null: {}", value)
-    value map parse
-  }
+  final override def asBulk[A: Parse]: Option[A] = value map implicitly[Parse[A]]
   final override def asString = value.get
 }
 
@@ -63,11 +49,7 @@ case class ErrorReply(value: RedisError) extends RedisReply[RedisError] {
 
 case class MultiBulkReply(value: List[BulkReply]) extends RedisReply[List[BulkReply]] {
 
-  final override def asList[A: Parse]: List[Option[A]] = {
-    val parse = implicitly[Parse[A]]
-    if (parse == null) log.warn("Implicit parse for {} in asList[T: Parse] is null", value)
-    value.map { _.value map parse }
-  }
+  final override def asList[A: Parse] = value.map { _.value map implicitly[Parse[A]] }
 
   final override def asListPairs[A: Parse, B: Parse]: List[Option[(A,B)]] = {
     val parseA = implicitly[Parse[A]]
