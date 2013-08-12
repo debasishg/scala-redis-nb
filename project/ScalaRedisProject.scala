9de97d0d64629055f0c964ad13883d49017fa5cb
guersam <xxxyel@gmail.com>
2013-08-13 00:49:59 +0900

    Bump version to avoid confliction

--- project/ScalaRedisProject.scala
+++ project/ScalaRedisProject.scala
@@ -8,7 +8,7 @@ object ScalaRedisProject extends Build
 
   lazy val commonSettings: Seq[Setting[_]] = Seq(
     organization := "net.debasishg",
-    version := "2.10",
+    version := "2.11-SNAPSHOT",
     scalaVersion := "2.10.2",
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:postfixOps"),
     resolvers ++= Seq(akkaRelease, akkaSnapshot)
