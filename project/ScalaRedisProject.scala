7a969bceb0eaed69314b0c022fbe86ceb058e3d1
Jisoo Park <xxxyel@gmail.com>
2014-03-12 11:48:14 +0900

    Bump version to 0.5-SNAPSHOT

--- project/ScalaRedisProject.scala
+++ project/ScalaRedisProject.scala
@@ -8,7 +8,7 @@ object ScalaRedisProject extends Build
 
   lazy val commonSettings: Seq[Setting[_]] = Seq(
     organization := "net.debasishg",
-    version := "0.4-SNAPSHOT",
+    version := "0.5-SNAPSHOT",
     scalaVersion := "2.10.4-RC3",
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:postfixOps"),
     resolvers ++= Seq(akkaRelease, akkaSnapshot, sprayJson)
