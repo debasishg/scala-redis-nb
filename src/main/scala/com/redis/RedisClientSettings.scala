77678463f24c78b6d77c08ad27a7329be84e1213
Logan Lowell <fractaloop@gmail.com>
2014-04-18 16:12:04 -0500

    Add some ReconnectionSettings, with Constant and Exponential policies

--- src/main/scala/com/redis/RedisClientSettings.scala
+++ src/main/scala/com/redis/RedisClientSettings.scala
@@ -4,7 +4,8 @@ import RedisClientSettings._
 
 
 case class RedisClientSettings(
-  backpressureBufferSettings: Option[BackpressureBufferSettings] = None
+  backpressureBufferSettings: Option[BackpressureBufferSettings] = None,
+  reconnectionSettings: Option[ReconnectionSettings] = None
 )
 
 object RedisClientSettings {
@@ -19,6 +20,39 @@ object RedisClientSettings {
     val default = BackpressureBufferSettings(lowBytes = 100, highBytes = 3000, maxBytes = 5000)
   }
 
+  trait ReconnectionSettings {
+    def newSchedule: ReconnectionSchedule
+
+    trait ReconnectionSchedule {
+      def nextDelayMs: Long
+    }
+  }
+
+  case class ConstantReconnectionSettings(constantDelayMs: Long) extends ReconnectionSettings {
+    require(constantDelayMs >= 0, s"Invalid negative reconnection delay (received $constantDelayMs)")
+
+    def newSchedule: ReconnectionSchedule = new ConstantSchedule
+
+    class ConstantSchedule extends ReconnectionSchedule {
+      def nextDelayMs = constantDelayMs
+    }
+  }
+
+  case class ExponentialReconnectionPolicy(baseDelayMs: Long, maxDelayMs: Long) extends ReconnectionSettings {
+    require(baseDelayMs > 0, s"Base reconnection delay must be greater than 0. Received $baseDelayMs")
+    require(maxDelayMs > 0, s"Maximum reconnection delay must be greater than 0. Received $maxDelayMs")
+    require(maxDelayMs >= baseDelayMs, "Maximum reconnection delay cannot be smaller than base reconnection delay")
+
+    def newSchedule = new ExponentialSchedule
+
+    class ExponentialSchedule extends ReconnectionSchedule {
+      var attempts = 0
+      def nextDelayMs = {
+        attempts += 1
+        Math.min(baseDelayMs * (1L << attempts), maxDelayMs)
+      }
+    }
+  }
 }
 
 
