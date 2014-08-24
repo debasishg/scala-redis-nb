fe43f24c895421a805c685408491cf6f2041c83e
debasishg <ghosh.debasish@gmail.com>
2014-08-24 21:34:11 +0530

    [Misc] Version upgraded to 0.6

--- project/ScalaRedisProject.scala
+++ project/ScalaRedisProject.scala
@@ -8,7 +8,7 @@ object ScalaRedisProject extends Build
 
   lazy val commonSettings: Seq[Setting[_]] = Seq(
     organization := "net.debasishg",
-    version := "0.6-SNAPSHOT",
+    version := "0.6",
     scalaVersion := "2.10.4",
     crossScalaVersions := Seq("2.10.4", "2.11.1"),
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:postfixOps"),
